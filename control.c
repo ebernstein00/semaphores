e9287c15fe9b2bec1525c5f7e94bd7dda5eebc29
Elias Bernstein <41696293+ebernstein00@users.noreply.github.com>
2019-12-25 12:32:28 -0500

    Create write.c

--- control.c
+++ control.c
@@ -1,13 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/shm.h>
-#include <sys/ipc.h>
-#include <sys/types.h>
-#include <sys/sem.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "semaphore.h"
 
 union semun {
             int val;
